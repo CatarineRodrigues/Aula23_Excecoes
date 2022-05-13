11adec0008f6b9b08bef01a591cd61acc72c446a
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-05-13 18:23:46 -0300

    Testando variacao do exercicio moedas

--- src/lista25_execoes/exerc3/Constantes.kt
+++ src/lista25_execoes/exerc3/Constantes.kt
@@ -1,5 +1,13 @@
 package lista25_execoes.exerc3
 
-const val MSG_TIPO_MOEDA = "Qual moeda deseja converter?\n Dólar americano, Dólar canadense, Euro ou Libra Esterlina?"
+const val MSG_TIPO_MOEDA = "Qual moeda deseja converter?\n1- Dólar americano, 2- Dólar canadense, 3- Euro ou 4- Libra Esterlina?"
 const val MSG_OPCAO_INVALIDA = "Opção Inválida"
-const val MSG_DESEJA_NOVA_COTACAO = "Deseja fazer outra conversão"
+const val MSG_DESEJA_NOVA_COTACAO = "Deseja fazer outra conversão?"
+const val DOLLAR_AMERICANO = "dólar americano"
+const val DOLLAR_CANADENSE = "dólar canadense"
+const val EURO = "euro"
+const val LIBRA = "libra esterlina"
+const val COTACAO_DOLLAR_AMERICANO = 5.14
+const val COTACAO_DOLLAR_CANADENSE = 3.96
+const val COTACAO_EURO = 5.41
+const val COTACAO_LIBRA_ESTERLINA = 6.29
--- src/lista25_execoes/exerc3/ConversorMoedas.kt
+++ src/lista25_execoes/exerc3/ConversorMoedas.kt
@@ -10,20 +10,28 @@ class ConversorMoedas {
         val tipoMoeda = pedirTipoMoeda()
         println("Lembrete: Este programa só converte para Real (R$)")
         val valorMoeda = armazenaValorMoeda()
-        val valorCotacao = valorContacaoMoedas(tipoMoeda)
+        val valorCotacao = valorContacaoMoedas(tipoMoeda.toString())
         val valorConvertido = converterMoedas(valorMoeda, valorCotacao)
-        exibirValorConvertido(tipoMoeda, valorCotacao, valorMoeda, valorConvertido)
+        exibirValorConvertido(tipoMoeda.toString(), valorCotacao, valorMoeda, valorConvertido)
         perguntaRodarNovamente()
     }
 
-    private fun pedirTipoMoeda(): String {
+    private fun pedirTipoMoeda() {
+        var tipoMoedaInvalida = false
+
         println(MSG_TIPO_MOEDA)
-        return try {
+        try {
             val tipoMoeda = readln().lowercase()
-            tipoMoeda
+            when {
+                tipoMoeda != "dólar americano" || tipoMoeda != "dólar canadense" || tipoMoeda != "euro" || tipoMoeda != "libra esterlina" -> {
+                    tipoMoedaInvalida = true
+                }
+            }
         } catch (exception: IllegalArgumentException) {
             println(exception.message)
-            println(MSG_OPCAO_INVALIDA)
+            if (tipoMoedaInvalida) {
+                println(MSG_OPCAO_INVALIDA)
+            }
             this.pedirTipoMoeda()
         }
     }
@@ -47,10 +55,10 @@ class ConversorMoedas {
     private fun valorContacaoMoedas(tipoMoeda: String): BigDecimal {
         var valorCotacao: BigDecimal = BigDecimal.ZERO
         when (tipoMoeda) {
-            "dólar americano" -> valorCotacao = BigDecimal(5.14)
-            "dólar canadense" -> valorCotacao = BigDecimal(3.96)
-            "euro" -> valorCotacao = BigDecimal(5.41)
-            "libra esterlina" -> valorCotacao = BigDecimal(6.29)
+            DOLLAR_AMERICANO -> valorCotacao = COTACAO_DOLLAR_AMERICANO.toBigDecimal()
+            DOLLAR_CANADENSE -> valorCotacao = COTACAO_DOLLAR_CANADENSE.toBigDecimal()
+            EURO -> valorCotacao = COTACAO_EURO.toBigDecimal()
+            LIBRA -> valorCotacao = COTACAO_LIBRA_ESTERLINA.toBigDecimal()
         }
         return valorCotacao
     }
@@ -63,7 +71,7 @@ class ConversorMoedas {
         valorMoeda: BigDecimal,
         valorConvertido: BigDecimal
     ) {
-        println("A moeda $tipoMoeda na cotação de hoje está em $valorCotacao, o valor que pediu para converter de $valorMoeda em reais é R$$valorConvertido")
+        println("A moeda $tipoMoeda na cotação de hoje está em $$valorCotacao, o valor que pediu para converter de $valorMoeda em reais é R$$valorConvertido")
     }
 
     private fun perguntaRodarNovamente() {
@@ -71,8 +79,8 @@ class ConversorMoedas {
             println(MSG_DESEJA_NOVA_COTACAO)
             println("1-SIM ou 2-NÃO/SAIR")
             when (readln().uppercase()) {
-                "1","SIM" -> inicioPrograma()
-                "2","NÃO","NAO","SAIR" -> {
+                "1", "SIM" -> inicioPrograma()
+                "2", "NÃO", "NAO", "SAIR" -> {
                     println("Obrigado por usar nosso sistema!")
                     exitProcess(0)
                 }
